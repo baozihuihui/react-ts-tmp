83fd1c1ebb077ecb82661e61a097dd9dad89ffcc
18094315 <18094315@suning.com>
2020-04-18 15:26:42 +0800

    feat/新增clean插件

--- build/webpack.config.prd.js
+++ build/webpack.config.prd.js
@@ -1,11 +1,12 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
 
 module.exports = {
     mode:'production',
     entry:'./src/index.js',
     output:{
-        path:'./dist/js',
+        path:path.join(__dirname,'./dist/js'),
         filename:'[name]-[hash].js'
     },
     resolve:{
