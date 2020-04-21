c94918ca7d0c9b9efce7057f924f832687c34793
18094315 <18094315@suning.com>
2020-04-21 10:10:19 +0800

    fix/antd样式未引入的问题

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -20,20 +20,41 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                exclude: /node_modules/, // exclude antd default style
+                // exclude: /node_modules/, // exclude antd default style
                 use: [util.loaders.styleLoader, util.loaders.cssLoader],
             },
             {
                 test: /\.less$/,
-                exclude: /node_modules/, // exclude antd default style
+                // exclude: /node_modules/, // exclude antd default style
                 use: [util.loaders.styleLoader, util.loaders.cssLoader, util.loaders.lessLoader],
             },
         ],
     },
     optimization: {
-        // 简单拆包
         splitChunks: {
             chunks: 'all',
+            // 如果项目依赖变多时，可以考虑采用下方更精细的分片规则
+            // minSize: 60000,
+            // minChunks: 3,
+            // maxAsyncRequests: 5,
+            // maxInitialRequests: 3,
+            // cacheGroups: {
+            //     antdUI: {
+            //         priority: 20,
+            //         name: 'antdUI',
+            //         test: /[\\/]node_modules[\\/]antd[\\/]/,
+            //     },
+            //     antDesign: {
+            //         priority: 20,
+            //         name: 'antdUI',
+            //         test: /[\\/]node_modules[\\/]@ant-design[\\/]/,
+            //     },
+            //     vendors: {
+            //         test: /[\\/]node_modules[\\/]/,
+            //         priority: -10,
+            //         name: 'vendors',
+            //     },
+            // },
         },
     },
 }
