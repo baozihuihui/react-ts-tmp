b2d7e66378f126a30fbb13bd5f5cb41e509d84f6
FattyCat <731493610@qq.com>
2021-04-03 12:13:13 +0800

    Spin 组件添加渲染子元素

--- src/common/components/Loadding/index.tsx
+++ src/common/components/Loadding/index.tsx
@@ -1,10 +1,14 @@
 import { Spin } from 'antd'
 import React from 'react'
 
-const Loadding: React.FC = () => {
+interface IProps {
+	children?: React.Component
+}
+
+const Loadding: React.FC<IProps> = (props: IProps) => {
 	return (
 		<div style={{ textAlign: 'center', margin: '20px 0' }}>
-			<Spin style={{ margin: '0 auto' }} />
+			<Spin style={{ margin: '0 auto' }}>{props.children}</Spin>
 		</div>
 	)
 }
