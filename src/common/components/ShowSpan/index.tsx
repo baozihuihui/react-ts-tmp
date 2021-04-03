4edb8a694970fe0d9b001a6df97362e669b71c5a
FattyCat <731493610@qq.com>
2021-04-03 21:41:12 +0800

    add showSpan description

--- src/common/components/ShowSpan/index.tsx
+++ src/common/components/ShowSpan/index.tsx
@@ -6,12 +6,13 @@ const defaultNumberEmptyValue = '-'
 interface IShowSpanProps {
 	value: string | number | null | undefined
 	emptyValue?: '' | '-' | '无' | '空'
+	description?: string // 单位 或 描述值
 	className?: string
 	style?: React.CSSProperties
 }
 
 const ShowSpan = (props: IShowSpanProps) => {
-	const { value, className, style, emptyValue } = props
+	const { value, emptyValue, className, style, description } = props
 	const valueType = typeof value
 	let result: string | number
 	let emptyValueWithType: string
@@ -31,6 +32,7 @@ const ShowSpan = (props: IShowSpanProps) => {
 	return (
 		<span className={className} style={style}>
 			{result}
+			{description}
 		</span>
 	)
 }
