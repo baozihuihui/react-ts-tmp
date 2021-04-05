9e0b76f67646a2aae6ab736d40bc681500db0a2f
FattyCat <731493610@qq.com>
2021-04-05 23:02:58 +0800

    add logs

--- src/pages/page1/App.tsx
+++ src/pages/page1/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
-// 引入第三方 IconFonts的样式表
-import '@assest/iconfont/iconfont.css'
-// 引入顶层错误边界
-import ErrorBoundry from '@common/components/ErrorBoundary'
-import configureStore, { history } from './redux/rootRedux'
-import routeInfos, { defaultPath } from './router/route.config'
-import Applayout from '@common/layout/Applayout'
 import { ConnectedRouter } from 'connected-react-router'
+// add IconFonts styles
+import '@assest/iconfont/iconfont.css'
+// catch ErroeBundry catch redner error and show Error Components
+import ErrorBoundry from '@common/components/ErrorBoundary'
+// redux
+import configureStore, { history } from './redux/rootRedux'
+// routeInfos
+import routeInfos, { defaultPath } from './router/route.config'
+// layout header-left_NavBar-right_content-footer
+import Applayout from '@common/layout/Applayout'
+
 export default class App extends React.Component {
 	store = configureStore()
 	render() {
