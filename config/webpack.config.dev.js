9f8d7138aec4e5e8fb2967a02c2f4262b5658b8b
郭星东 <xingdongguo@linkdoc.com>
2022-02-23 16:34:23 +0800

    add/更新版本依赖

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -12,22 +12,27 @@ module.exports = {
 	// 入口文件 ，若多入口，则数组表示多个入口
 	entry: ['./src/pages/page1/index.dev.tsx'],
 	output: {
-		publicPath: '/', // app.js => /app.js for H5 HistoryAPI staticPath
+		publicPath: path.join(__dirname, '/'), // app.js => /app.js for H5 HistoryAPI staticPath
 	},
 	// 开发环境我们只添加  忽略列信息的ts源码的sourcemap
-	devtool: 'cheap-module-eval-source-map',
+	devtool: 'eval-source-map',
 	// 本地服务器
 	devServer: {
-		contentBase: path.join(__dirname, './src/'),
-		publicPath: '/',
-		host: '127.0.0.1',
-		port: 3000,
-		hot: true,
-		stats: {
-			colors: true,
+		// 静态资源访问配置
+		static: {
+			directory: path.join(__dirname, '/'),
 		},
+		client: {
+			logging: 'info', // log 打印类型
+			overlay: true, // 编译失败 全屏展示错误信息
+			progress: true, // 编译进度
+		},
+		host: '0.0.0.0', // 允许外部访问服务
+		port: 3000, // 端口
+		hot: true, // 是否允许热更新
+		compress: true, // 启用gzip压缩
 		historyApiFallback: true, // H5 historyAPI
-		disableHostCheck: true, // 启动的项目配置host 允许访问
+		allowedHosts: 'all', // 启动的项目配置host 允许访问
 	},
 	// 引用的loader
 	module: {
